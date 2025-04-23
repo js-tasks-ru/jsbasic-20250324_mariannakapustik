b2f8eea0a894a9644ff084a3e0152b4a3a2b8319
mona-moka <mkapustik@outlook.com>
2025-04-24 02:46:36 +0300

    4-5-modules

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement("ul");
+
+  let li;
+
+  for (const friend of friends) {
+    li = document.createElement("li");
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.append(li);
+  }
+
+  return ul;
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight = document.querySelector(".carousel__arrow_right");
+  const arrowLeft = document.querySelector(".carousel__arrow_left");
+
+  const width = document.querySelector(".carousel__slide").offsetWidth;
+  const carousel = document.querySelector(".carousel__inner");
+
+  let i = 1;
+  let j = 1;
+
+  arrowLeft.style.display = "none";
+
+  arrowRight.addEventListener("click", () => {
+    arrowLeft.style.display = "";
+
+    carousel.style.transform = `translateX(-${width * i}px)`;
+    i++;
+    j--;
+
+    if (i === 4) {
+      arrowRight.style.display = "none";
+    }
+  });
+
+  arrowLeft.addEventListener("click", () => {
+    arrowRight.style.display = "";
+
+    carousel.style.transform = `translateX(${width * j}px)`;
+    j++;
+    i--;
+
+    if (j === 1) {
+      arrowLeft.style.display = "none";
+    }
+  });
 }
